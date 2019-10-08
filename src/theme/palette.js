9f7a46daab3e32187aeb67ce02a1f5fb9ee24ee3
Romina Moreno <arominamoreno@gmail.com>
2019-10-08 00:10:40 -0300

    Cambie el color de la paleta.

--- src/theme/palette.js
+++ src/theme/palette.js
@@ -8,15 +8,15 @@ export default {
   white,
   primary: {
     contrastText: white,
-    dark: colors.indigo[900],
-    main: colors.indigo[500],
-    light: colors.indigo[100]
+    dark: '#004838',
+    main: '#016751',
+    light: '#338573'
   },
   secondary: {
     contrastText: white,
-    dark: colors.blue[900],
-    main: colors.blue['A400'],
-    light: colors.blue['A400']
+    dark: '#585b5b',
+    main: '#7E8283',
+    light: '#979b9b'
   },
   success: {
     contrastText: white,
